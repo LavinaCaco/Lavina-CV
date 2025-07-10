c10fff84bbef26d86292f99b525d6c2ea80eb74c
LavinaCaco <cacotugas@gmail.com>
2025-07-10 22:50:54 +0700

    update cv

--- backend/data.js
+++ backend/data.js
@@ -3,17 +3,16 @@ const educationHistory = [
     { id: 2, period: '2019 - 2022', institution: 'SMK Muhammadiyah 1  Yogyakarta', major: 'Rekayasa Perangkat Lunak' }
   ];
   
-  const skills = [
-    { name: 'React.js', level: 'Mahir' },
-    { name: 'CodeIgniter', level: 'Mahir' },
-    { name: 'Laravel', level: 'Mahir' },
-    { name: 'Tailwind CSS', level: 'Menengah' },
-    { name: 'Bootstrap CSS', level: 'Menengah' },
-    { name: 'MySql', level: 'Menengah' },
-    { name: 'Git & GitHub', level: 'Mahir' },
-    { name: 'HTML5 & CSS3', level: 'Mahir' }
-  ];
-  
+const skills = [
+  { name: 'React.js', level: 'Mahir', iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'},
+  { name: 'CodeIgniter', level: 'Mahir', iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/codeigniter/codeigniter-plain.svg'},
+  { name: 'Laravel', level: 'Mahir', iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-original.svg'},
+  { name: 'Tailwind CSS', level: 'Menengah', iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg'},
+  { name: 'Bootstrap CSS', level: 'Menengah', iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg'},
+  { name: 'MySql', level: 'Menengah', iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg'},
+  { name: 'Git & GitHub', level: 'Mahir', iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg'},
+  { name: 'HTML5 & CSS3', level: 'Mahir', iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'}
+];
   const projects = [
     {
       title: 'Website RentalIqra',
